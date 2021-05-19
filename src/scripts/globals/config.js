ac334ad4a034192b084c944a08c0ab3f825e6529
HendP <hendpermana12@gmail.com>
2021-05-20 06:41:46 +0700

    add fetch api and display data

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    KEY: '7d3f1a6b76c8c205e8f09069a64e2346',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
